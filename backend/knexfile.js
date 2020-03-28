337d1634d2b89fe1788e2238931468e6eaa81e97
Christian Toledo <christian.odt@gmail.com>
2020-03-27 21:55:40 -0300

    add seeds

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -9,6 +9,9 @@ module.exports = {
     migrations: {
       directory: './src/database/migrations',
     },
+    seeds: {
+      directory: './src/database/seeds',
+    },
     useNullAsDefault: true,
   },
 
